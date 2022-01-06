c65594af18df367e2af9f41b4fbe898cc2ac4fb8
DinossauroBebado <guipsilva32@gmail.com>
2022-01-05 21:07:36 -0300

    that's it

--- four_sum_up.py
+++ four_sum_up.py
@@ -1,10 +1,39 @@
-from bottle import jinja2_view, route, run, request, response
+from bottle import Route, jinja2_view, route, run, request, response
 
 
-@route('/ <area>')
-@jinja2_view('paginas\teste.html')
+@route('/<area>')
+@jinja2_view('paginas/teste.html')
 def teste(area):
+    """Rederizando uma view com o decorador do jinja."""
+
     return dict(nome=area)
+
+
+@route('/user', method="GET")
+@jinja2_view('paginas/user.html')
+def user():
+    links = ['home', 'about', 'help']
+
+    return dict(links=links)
+
+
+@route('/user', method='POST')
+def user_post():
+    """Uma página básica de login que usa cookies."""
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    if username == 'eduardo' and password == 'senha' or \
+       request.get_cookie("user") == 'eduardo':
+
+        response.set_cookie("user", "eduardo")
+        links = ['home', 'user_space', 'metrics', 'help']
+        return dict(string='Você está logado', links=links)
+    else:
+        response.set_cookie("user", "None")
+        return """
+        <h1> ERRO ao efetuar loggin</h1>
+        """
 
 
 run(port=8080)
